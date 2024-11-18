5d52873d16445eb470be477352ad71b6d5254f3e
Alonso Díaz <136006184+Vestermix@users.noreply.github.com>
2024-11-18 04:53:39 -0300

    Eliminación key base de datos

--- ArchivosProjectoDjango/ProjectoWebISO/settings.py
+++ ArchivosProjectoDjango/ProjectoWebISO/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r%@b&3+0@1fkgyln6wx+q4j-+zrs%4iq+m7-de4m@jedna+70e'
+SECRET_KEY = ''  # Cambiar por una clave secreta
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
